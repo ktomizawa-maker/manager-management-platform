cf547d79b16dba65520ff80b30b6200043f4c98a
ktomizawa-maker <k.tomizawa@idea-nov.com>
2026-07-04 16:11:30 +0900

    Update management-platform/config.js

--- management-platform/config.js
+++ management-platform/config.js
@@ -1,4 +1,4 @@
-window.managementPlatformConfig = {
+﻿window.managementPlatformConfig = {
   supabaseUrl: "https://nkmxevmioczcmnldreyo.supabase.co",
   supabaseAnonKey: "sb_publishable_2JEtgHNrcVg3mQVgfpxT3g_8Gan4Tsq",
   edgeFunctionName: "manager-platform-summary",
